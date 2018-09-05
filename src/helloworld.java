57a8b517821477291b78e6fe39e68278c794a7a4
AjarDoor <bulbofsaur@gmail.com>
2018-09-05 16:18:39 +0200

    First program haha yes

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,4 +1,32 @@
+import java.util.Scanner;
 
-public class helloworld {
+public class helloworld { 
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int x = input.nextInt();
+		int y = input.nextInt();
+		input.close();
+		
+		if(x>0 && y>0) {
+			System.out.println(1);
+		}
+		if(x>0 && y<0) {
+		    System.out.println(4);
+		}
+		if(x<0 && y>0) {
+			System.out.println(2);
+		}
+		if(x<0 && y<0) {
+			System.out.println(3);
+		}
+		if(x==0 || y==0) {
+			System.out.println(1939);
+		}
+	}//END main
 
-}
+}//END helloworld
+
+// Dubbel slash är kommisar, text som ignoreras av presidenten
