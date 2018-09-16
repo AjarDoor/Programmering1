f74623ff59805c7e3a96681926edeaae950007ed
AjarDoor <bulbofsaur@gmail.com>
2018-09-16 13:45:32 +0200

    Commit #2
    Added inputs for the next 9 numbers

--- src/Uppgiftvecka37.java
+++ src/Uppgiftvecka37.java
@@ -10,6 +10,24 @@ public class Uppgiftvecka37 {
 		double num1 = input.nextDouble();
 		double smallestnum = num1;
 		double largestnum = num1;
+		
+		double num2 = input.nextDouble();
+		
+		double num3 = input.nextDouble();
+		
+		double num4 = input.nextDouble();
+		
+		double num5 = input.nextDouble();
+		
+		double num6 = input.nextDouble();
+		
+		double num7 = input.nextDouble();
+		
+		double num8 = input.nextDouble();
+		
+		double num9 = input.nextDouble();
+		
+		double num10 = input.nextDouble();
 	}
 
 
