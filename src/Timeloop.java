ebcdd9375f840d9dd0555d02ebafee9a31fd0818
AjarDoor <bulbofsaur@gmail.com>
2018-09-21 14:57:21 +0200

    Removed Comment

--- src/Timeloop.java
+++ src/Timeloop.java
@@ -7,7 +7,6 @@ public class Timeloop {
 		Scanner input = new Scanner(System.in);
 
 		int num = input.nextInt();
-		// Skriver ut 1-num
 		for (int i = 1; i <= num; i++) {
 			System.out.println(i + " Abracadabra");
 		}
