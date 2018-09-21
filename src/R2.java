11d5f1e91b698bc16362bbca1db9c9de47d7225a
AjarDoor <bulbofsaur@gmail.com>
2018-09-21 14:30:57 +0200

    Cleaned up

--- src/R2.java
+++ src/R2.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
 
 public class R2 {
-	
+
 	public static void main(String[] args) {
 		Scanner Input = new Scanner(System.in);
-	int R1 = Input.nextInt();
-	
-	int S = Input.nextInt();
-	
-	int R2 = (2*S - R1);
+		int R1 = Input.nextInt();
 
-			
-	Input.close();
-	System.out.println(R2);
-		
-		
-		
+		int S = Input.nextInt();
+
+		int R2 = (2 * S - R1);
+
+		Input.close();
+		System.out.println(R2);
+
 	}
 
 }
