694f17a6873389609874b9e5500c2482f058734e
AjarDoor <bulbofsaur@gmail.com>
2018-10-10 16:08:21 +0200

    methods

--- src/repitition/rep4.java
+++ src/repitition/rep4.java
@@ -6,13 +6,15 @@ public class rep4 {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
-
+		System.out.println("Input tal mellan 1-10 för att få dess gångertabell");
 		int tal = input.nextInt();
 
-		for (int i = 1; i <= 10; i++) {
-			int sum = tal * i;
-			System.out.println(tal + " gånger " + i + " är lika med " + sum);
-
+		if (tal > 0 && tal <= 10) {
+			for (int i = 1; i != 11; i++) {
+				System.out.println(tal + " gånger " + i + " är lika med " + tal * i);
+			}
+		} else {
+			input.close();
 		}
 		input.close();
 	}
